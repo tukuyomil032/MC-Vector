bab7b96d11713abdcfef548140b97fd64e7a30b6
tukuyomil032 <yinkom0708@gmail.com>
2026-09-09 12:00:17 +0900

    ref: add authoritative server operation boundary

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 mod commands;
+mod state;
 
 use tauri::Manager;
 
@@ -50,6 +51,7 @@ pub fn run() {
         })
         .manage(commands::server::ServerManager::default())
         .manage(commands::server::CommandLimiter::default())
+        .manage(state::operation_manager::ServerOperationManager::default())
         .manage(commands::ngrok::NgrokManager::default())
         .manage(commands::file_utils::ServerImportManager::default())
         .manage(commands::health_check::ProbeLimiter::default())
